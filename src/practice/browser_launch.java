d5112e7bb9a0bdc148c6ffada9fc30e27c7ad8bb
Shashikant Kokitakar <shashikokitakar95@gmail.com>
2022-12-25 21:47:50 +0530

    sdet-1 added new function

--- src/practice/browser_launch.java
+++ src/practice/browser_launch.java
@@ -5,12 +5,15 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class browser_launch {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException 
 	{
 		// TODO Auto-generated method stub
 		 System.setProperty("webdriver.gecko.driver", "D:\\Selenium material\\Selenium project\\Ezymech\\Drivers\\geckodriver.exe");
 		  WebDriver driver=new FirefoxDriver();
-		  driver.get("https://ezy.herokuapp.com/"); 
+		  driver.get("https://ezy.herokuapp.com/");
+		  //updating the code
+		  Thread.sleep(3000);
+		  driver.quit();
 
 	}
 
