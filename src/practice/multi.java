9bf4a8024a88617bedf48ee8794886d624e4681c
Shashikant Kokitakar <shashikokitakar95@gmail.com>
2022-12-25 21:34:02 +0530

    updated by sdet-1

---
+++ src/practice/multi.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class multi {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int a=20;
+		int b=30;
+		int c;
+		System.out.println(c=b*a);
+
+	}
+
+}
